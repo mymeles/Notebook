d3e8c17a2736192b5d0e6401742c36ac812dad4c
mymeles <mymeles@ncsu.edu>
2021-04-09 17:58:30 -0400

    [Implements] Implemented ActiveTaskList -pair Programming with Helen solomon

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/tasks/ActiveTaskList.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/tasks/ActiveTaskList.java
@@ -1,7 +1,10 @@
 package edu.ncsu.csc216.wolf_tasks.model.tasks;
 
+import edu.ncsu.csc216.wolf_tasks.model.util.ISwapList;
+
 /**
  * Class for the list of active tasks
+ * 
  * @author Helen Solomon
  * @author Meles Meles
  *
@@ -11,46 +14,68 @@ public class ActiveTaskList extends AbstractTaskList {
 	/** name of active task list */
 	public static final String ACTIVE_TASKS_NAME = "Active Tasks";
 
-	
 	/**
 	 * Constructor for the active task list
-	 * @param taskListName name of task list
+	 * 
+	 * @param taskListName   name of task list
 	 * @param completedCount number of completed tasks
 	 */
 	public ActiveTaskList() {
-     	super(ACTIVE_TASKS_NAME, 0);
- 
+		super(ACTIVE_TASKS_NAME, 0);
+
 	}
 
 	/**
 	 * Adds the given task to the active task list
+	 * 
 	 * @param task task to be added to list
+	 * 
+	 * @throws IllegalArgumentException if task is not active.
 	 */
+	@Override
 	public void addTask(Task task) {
-		
+		if (!task.isActive()) {
+			throw new IllegalArgumentException("Cannot add task to Active Tasks.");
+		}
+		super.addTask(task);
+
 	}
-	
+
 	/**
-	 * Sets the name of the active task list
+	 * ?????????????????????????????????????????????????????? Sets the name of the
+	 * active task list
 	 */
+	@Override
 	public void setTaskListName(String taskListName) {
-		
+		if (taskListName == ACTIVE_TASKS_NAME) { 
+			super.setTaskListName(taskListName);
+		} else {
+			throw new IllegalArgumentException("The Active Tasks list may not be edited.");
+		}
 	}
-	
+
 	/**
-	 *  Creates a 2D string array of tasks in the active task list
-	 *  @return 2D array of tasks in list
+	 * Creates a 2D string array of tasks in the active task list
+	 * 
+	 * @return 2D array of tasks in list
 	 */
 	public String[][] getTasksAsArray() {
-		return null;
+		String[][] taskArray = new String[super.getTasks().size()][2];
+		for (int i = 0; i < super.getTasks().size(); i++) {
+			taskArray[i][0] = super.getTasks().get(i).getTaskListName();
+			taskArray[i][1] = super.getTasks().get(i).getTaskName();
+		}
+		return taskArray;
 	}
-	
+
 	/**
 	 * Clears the ActiveTaskList of all Tasks
 	 */
 	public void clearTasks() {
-		
+
+		for (int i = super.getTasks().size() - 1; i <= 0; i--) {
+			super.getTasks().remove(i);
+		}
 	}
-	
+
 }
- 
