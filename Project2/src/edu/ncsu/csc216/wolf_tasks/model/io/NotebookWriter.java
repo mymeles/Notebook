03a5eac7f57b39e4a751bbae1b8845b6bd16a410
hmsolomo <hmsolomo@ncsu.edu>
2021-04-14 18:37:26 -0400

    [Debug] Fix output, add newline at beginning instead of end of line, adjust tests accordingly

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookWriter.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookWriter.java
@@ -20,19 +20,20 @@ public class NotebookWriter {
 	/**
 	 * A method that write Task List under a given Notebook to a give file name location.
 	 * @param file location to write the NootBook
-	 * @param notebook A string that holds Notebook name 
+	 * @param name A string that holds Notebook name 
 	 * @param taskList a list of task that are under Notebook
 	 */
-	public static void writeNotebookFile(File file, String notebookName, ISortedList<TaskList> taskList) {
+	public static void writeNotebookFile(File file, String name, ISortedList<TaskList> taskList) {
 		try {
 			PrintStream fileWriter = new PrintStream(file);
-			fileWriter.println("! " + notebookName);
+			fileWriter.print("! " + name);
+			//print out information of activetasklist
 			for (int i = 0; i < taskList.size(); i++) {
 				//first print the name of the task list
 				//start a for loop that prints all the tasklists and their tasks
-				fileWriter.println("# " + taskList.get(i).getTaskListName());
+				fileWriter.print("\n# " + taskList.get(i).getTaskListName() + "," + taskList.get(i).getCompletedCount());
 				for (int j = 0; j < taskList.get(i).getTasks().size(); j++) {
-					fileWriter.println(taskList.get(i).getTasks().get(j).toString().trim());
+					fileWriter.print("\n" + taskList.get(i).getTasks().get(j).toString().trim());
 				}
 			}
 			fileWriter.close();
--- Project2/test/edu/ncsu/csc216/wolf_tasks/model/io/NotebookWriterTest.java
+++ Project2/test/edu/ncsu/csc216/wolf_tasks/model/io/NotebookWriterTest.java
@@ -6,22 +6,58 @@ import java.io.File;
 
 import org.junit.Test;
 
+import edu.ncsu.csc216.wolf_tasks.model.notebook.Notebook;
+import edu.ncsu.csc216.wolf_tasks.model.tasks.Task;
 import edu.ncsu.csc216.wolf_tasks.model.tasks.TaskList;
 import edu.ncsu.csc216.wolf_tasks.model.util.SortedList;
 
+/**
+ * Tests for NotebookWriter class
+ * @author Helen Solomon
+ * @author Meles Meles
+ *
+ */
 public class NotebookWriterTest {
 
+	/**
+	 * Tests the writeNotebookFile() method
+	 */
 	@Test
 	public void testWriteNotebookFile() {
-		//File exp_file = new File("test-files/notebook1.txt");
-		File act_file = new File("test-files/notebook9.txt");
+		//file to print to
+		File actFile = new File("test-files/notebook9.txt");
 		SortedList<TaskList> sl = new SortedList<TaskList>();
+		TaskList list1 = new TaskList("ATaskList", 0);
+		TaskList list2 = new TaskList("Tasks1", 0);
+		TaskList list3 = new TaskList("Tasks2", 0);
+		Task task1 = new Task("Task1", "Task1Description", true, false);
+		Task task2 = new Task("Task2", "Task2Description", true, true);
+		Task task3 = new Task("Task3", "Task3Description", false, false);
+		Task task4 = new Task("Task4", "Task4Description", false, true);
+		Task task5 = new Task("Task4", "Task5Description", true, false);
+		list2.addTask(task1);
+		list2.addTask(task2);
+		list3.addTask(task3);
+		list3.addTask(task4);
+		list3.addTask(task5);
 		
-		NotebookWriter.writeNotebookFile(act_file, "Notebook", sl);
-		
-		assertEquals(0, NotebookReader.readNodebookFile(act_file).getTaskListsNames().length);
+		sl.add(list1);
+		sl.add(list2);
+		sl.add(list3);
 
-		//???????????
+		assertEquals(0, list1.getTasks().size());
+		assertEquals(2, list2.getTasks().size());
+		assertEquals(3, list3.getTasks().size());
+		assertEquals(3, sl.size());
+
+		NotebookWriter.writeNotebookFile(actFile, "Notebook", sl);
+		Notebook nb = NotebookReader.readNodebookFile(actFile);
+		
+		assertEquals(4, nb.getTaskListsNames().length);
+		for (int i = 0; i < sl.size(); i++) {
+			//check that the file contents are identical
+			assertEquals(nb.getTaskListsNames()[i + 1], sl.get(i).getTaskListName());
+		}
 	}
 
 }
